04cf027873ea528daa10023a69bacb439f1e2287
Robert Watt <wattrg@hotmail.com>
2022-07-12 09:42:14 +1000

    Properly initialise conserved quantities

--- src/conserved_quantities.cpp
+++ src/conserved_quantities.cpp
@@ -4,4 +4,6 @@ ConservedQuantity::ConservedQuantity() : _energy_idx(-1) {}
 
 ConservedQuantity::ConservedQuantity(GlobalConfig & config){
     this->_energy_idx = this->_momentum_idx + config.dimensions;
+    this->_n_conserved_quantities = _energy_idx + 1;
+    conserved_quantities = std::vector<double>(this->_n_conserved_quantities);
 }
--- src/conserved_quantities.h
+++ src/conserved_quantities.h
@@ -13,11 +13,13 @@ public:
     int rho() {return conserved_quantities[_rho_idx];}
     int momentum() {return conserved_quantities[_momentum_idx];}
     int energy() { return conserved_quantities[_energy_idx]; }
+    int n_conserved_quantities() {return _n_conserved_quantities; }
 
 private:
     int _rho_idx = 0;
     int _momentum_idx = 1;
     int _energy_idx;
+    int _n_conserved_quantities;
 };
 
 #endif // __CONSERVED_QUANTITIES_H_
